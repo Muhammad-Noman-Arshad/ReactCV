ded146df228126b084e80f7abec08236322a4211
Chaudhary Noman Arshad <numannch@gmail.com>
2024-05-30 13:58:32 +0500

    init basic app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import"./App.css"
+function App() {
+  return (
+    <div>
+      <h1 className="main-heading">Picture of Chaudhary Noman Arshad </h1>
+      <hr></hr>
+<img src="FB_IMG_15895729352405234.jpg" alt="Girl in a jacket" width="300" height="400">
+</img>
+</div>
+  );
+}
+export default App;  
