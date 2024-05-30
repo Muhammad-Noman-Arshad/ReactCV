35cdcdf93f25731001faa40a9c3a44075815ded3
Chaudhary Noman Arshad <numannch@gmail.com>
2024-05-30 15:59:40 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import"./App.css"
 function App() {
   return (
     <div>
-      <h1 className="main-heading">Picture of Chaudhary Noman Arshad </h1>
+      <h1 className="main-heading">A Picture of Chaudhary Noman Arshad </h1>
       <hr></hr>
 <img src="FB_IMG_15895729352405234.jpg" alt="Girl in a jacket" width="300" height="400">
 </img>
